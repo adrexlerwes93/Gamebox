95627dcccc589cc93befde72c4497494eb4cada6
Alex Drexler <alexdrexler@alexs-mbp.lan>
2017-08-18 23:11:50 -0700

    Adding color to dots game!

--- games/dots.rb
+++ games/dots.rb
@@ -1,5 +1,92 @@
 require 'io/console'
 
+class String
+  $STRING_COLORS=10
+  $BACKGROUND_COLORS=8
+  ##http://misc.flogisoft.com/bash/tip_colors_and_formatting
+  def black;          "\e[30m#{self}\e[0m" end
+  def red;            "\e[31m#{self}\e[0m" end
+  def green;          "\e[32m#{self}\e[0m" end
+  def brown;          "\e[33m#{self}\e[0m" end
+  def blue;           "\e[34m#{self}\e[0m" end
+  def magenta;        "\e[35m#{self}\e[0m" end
+  def cyan;           "\e[36m#{self}\e[0m" end
+  def gray;           "\e[37m#{self}\e[0m" end
+  def yellow;     "\e[93m#{self}\e[0m" end
+  def white;      "\e[97m#{self}\e[0m" end
+
+  def set_color(color)
+    case color
+    when "black"
+      "\e[30m#{self}\e[0m"
+    when "red"
+      "\e[31m#{self}\e[0m"
+    when "green"
+      "\e[32m#{self}\e[0m"
+    when "brown"
+      "\e[33m#{self}\e[0m"
+    when "blue"
+      "\e[34m#{self}\e[0m"
+    when "magenta"
+      "\e[35m#{self}\e[0m"
+    when "cyan"
+      "\e[36m#{self}\e[0m"
+    when "gray"
+      "\e[37m#{self}\e[0m"
+    when "yellow"
+      "\e[93m#{self}\e[0m"
+    when "white"
+      "\e[97m#{self}\e[0m"
+    end
+  end
+
+  #colorize background
+  def bg_black;       "\e[40m#{self}\e[0m" end
+  def bg_red;         "\e[41m#{self}\e[0m" end
+  def bg_green;       "\e[42m#{self}\e[0m" end
+  def bg_brown;       "\e[43m#{self}\e[0m" end
+  def bg_blue;        "\e[44m#{self}\e[0m" end
+  def bg_magenta;     "\e[45m#{self}\e[0m" end
+  def bg_cyan;        "\e[46m#{self}\e[0m" end
+  def bg_light_gray;  "\e[47m#{self}\e[0m" end
+  def bg_gray;    "\e[100m#{self}\e[0m"end
+  def bg_yellow;    "\e[103m#{self}\e[0m"end
+  def bg_white;   "\e[107m#{self}\e[0m"end
+
+  def set_bgcolor(color)
+    case color
+    when "black"
+      "\e[40m#{self}\e[0m"
+    when "red"
+      "\e[41m#{self}\e[0m"
+    when "green"
+      "\e[42m#{self}\e[0m"
+    when "brown"
+      "\e[43m#{self}\e[0m"
+    when "blue"
+      "\e[44m#{self}\e[0m"
+    when "magenta"
+      "\e[45m#{self}\e[0m"
+    when "cyan"
+      "\e[46m#{self}\e[0m"
+    when "light gray"
+      "\e[47m#{self}\e[0m"
+    when "gray"
+      "\e[100m#{self}\e[0m"
+    when "yellow"
+      "\e[103m#{self}\e[0m"
+    when "white"
+      "\e[107m#{self}\e[0m"
+    end
+  end
+  #stylize text
+  def bold;           "\e[1m#{self}\e[22m" end
+  def italic;         "\e[3m#{self}\e[23m" end
+  def underline;      "\e[4m#{self}\e[24m" end
+  def blink;          "\e[5m#{self}\e[25m" end
+  def reverse_color;  "\e[7m#{self}\e[27m" end
+end
+
 #reads single input from terminal
 def read_char
 	STDIN.echo = false
@@ -55,6 +142,23 @@ def createGame
   return [board,cursor,selected,0,20,0]
 end
 
+def printChar(char, color)
+  case color
+  when 'GRAY'
+    print char.bg_light_gray
+  when 'GREEN'
+    print char.bg_green
+  when 'YELLOW'
+    print char.bg_yellow
+  when 'CYAN'
+    print char.bg_cyan
+  when 'MAGENTA'
+    print char.bg_magenta
+  else
+    print char
+  end
+end
+
 def printBoard(game)
   board = game[0]
   cursor = game[1]
@@ -63,43 +167,49 @@ def printBoard(game)
   (0..9).each do |i|
     print "|"
     (0..9).each do |j|
-      if cursor[0] == i && cursor[1] == j && inSelection(selected,i,j)
-        print "{"
-      elsif cursor[0] == i && cursor[1] == j
-        print "<"
-      elsif inSelection(selected,i,j)
-        if game[3] == 1
-          print "["
-        else
-          print "("
-        end
-      else
-        print " "
-      end
       case board[i][j]
       when 1
-        print "@"
+        char = "@"
+        color = 'GRAY'
       when 2
-        print "!"
+        char = "!"
+        color = 'GREEN'
       when 3
-        print "%"
+        char = "%"
+        color = 'YELLOW'
       when 4
-        print "&"
+        char = "&"
+        color = 'CYAN'
       when 5
-        print "#"
+        char = "#"
+        color = 'MAGENTA'
       end
       if cursor[0] == i && cursor[1] == j && inSelection(selected,i,j)
-        print "}"
+        printChar("{", color)
       elsif cursor[0] == i && cursor[1] == j
-        print ">"
+        printChar("<", color)
       elsif inSelection(selected,i,j)
         if game[3] == 1
-          print "]"
+          printChar("[", color)
         else
-          print ")"
+          printChar("(", color)
         end
       else
-        print " "
+        printChar(" ", color)
+      end
+      printChar(char, color)
+      if cursor[0] == i && cursor[1] == j && inSelection(selected,i,j)
+        printChar("}", color)
+      elsif cursor[0] == i && cursor[1] == j
+        printChar(">", color)
+      elsif inSelection(selected,i,j)
+        if game[3] == 1
+          printChar("]", color)
+        else
+          printChar(")", color)
+        end
+      else
+        printChar(" ", color)
       end
     end
     puts "|"
